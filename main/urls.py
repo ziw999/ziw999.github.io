50d5ba7d014b0d6523b6638308132976a90695a9
ziw999 <155981246+ziw999@users.noreply.github.com>
2024-12-04 15:52:36 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contacts/', views.contacts, name='contacts'),
     path('menu/', views.menu, name='menu')
